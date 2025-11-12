4d1419603b0d834170f92f25102f13443fdb680c
hingobway <hingobway@users.noreply.github.com>
2025-11-11 23:51:00 -0500

    working validation

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
+#include <fstream>
 #include <iostream>
+#include <jsoncons/json.hpp>
+#include <jsoncons_ext/jsonpatch/jsonpatch.hpp>
+#include <jsoncons_ext/jsonschema/json_schema.hpp>
+#include <jsoncons_ext/jsonschema/json_schema_factory.hpp>
+#include <sstream>
+#include <stdexcept>
 #include <string>
+#include <utility>
 
 #include "scgen.h"
 
-int main()
-{
-  std::cout << "hi\n";
+using jsoncons::ojson;
+namespace jsonschema = jsoncons::jsonschema;
 
-  std::string text = R"(
+std::string text = R"(
 {
   "person": {
     "firstName": "Michael",
@@ -20,11 +27,43 @@ int main()
 }
 )";
 
+static std::string read_file_to_string(const std::string& path) {
+  std::ifstream in(path, std::ios::binary);
+  if (!in) throw std::runtime_error("Unable to open file: " + path);
+  std::ostringstream ss;
+  ss << in.rdbuf();
+  return ss.str();
+}
+int structure_and_print();
+
+const std::string tracker_json = read_file_to_string("../schema/tracker.json");
+
+int main() {
+  ojson schema = ojson::parse(tracker_json);
+  jsonschema::json_schema<ojson> compiled =
+      jsonschema::make_json_schema(std::move(schema));
+
+  try {
+    ojson data = ojson::parse(text);
+    compiled.validate(data);
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+
+  return structure_and_print();
+}
+
+int structure_and_print() {
+  std::cout << "validation success!\n";
+
   schema::Tracker abc = schema::json::parse(text);
 
   std::cout << "First Name: " << abc.person.first_name << "\n";
-  std::cout << "Last Name: " << abc.person.last_name.value_or("** NO LAST NAME") << "\n";
-  std::cout << "Last Name: " << abc.person.email.value_or("** NO EMAIL") << "\n";
+  std::cout << "Last Name: " << abc.person.last_name.value_or("** NO LAST NAME")
+            << "\n";
+  std::cout << "Last Name: " << abc.person.email.value_or("** NO EMAIL")
+            << "\n";
   std::cout << "Latitude: " << abc.loc.lat << "\n";
   std::cout << "Longitude: " << abc.loc.lon << "\n";
 
